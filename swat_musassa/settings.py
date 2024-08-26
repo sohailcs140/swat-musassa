c384afd6f857311d5840be4a51ad1edee9be9115
Sohail Ahmad <sohailcs140>
2024-08-26 14:08:23 +0500

    update database country field length

--- swat_musassa/settings.py
+++ swat_musassa/settings.py
@@ -70,23 +70,23 @@ WSGI_APPLICATION = 'swat_musassa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydatabase',  # Replace with your database name
-        'USER': 'root',  # Replace with your MySQL username
-        'PASSWORD': 'root',  # Replace with your MySQL password
-        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
-        'PORT': '3306',  # Default MySQL port
+        'NAME': 'mydatabase',
+        'USER': 'root', 
+        'PASSWORD': 'root', 
+        'HOST': 'localhost',  
+        'PORT': '3306',  
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
